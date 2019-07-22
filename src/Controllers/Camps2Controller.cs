440d724af76a77ea8e51ac1357cbaca2b3be151a
hkirfan <hkirfan@hsc.wvu.edu>
2019-07-22 12:18:14 -0400

    Implemented Functional API and wrapping up with Versioning.

--- src/Controllers/Camps2Controller.cs
+++ src/Controllers/Camps2Controller.cs
@@ -11,17 +11,16 @@ using Microsoft.AspNetCore.Routing;
 
 namespace CoreCodeCamp.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiVersion("1.0")]
-    [ApiVersion("1.1")]
+    [Route("api/camps")]
+    [ApiVersion("2.0")]
     [ApiController]
-    public class CampsController : ControllerBase
+    public class Camps2Controller : ControllerBase
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
         private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
+        public Camps2Controller(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _repository = repository;
             _mapper = mapper;
@@ -29,12 +28,17 @@ namespace CoreCodeCamp.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
+        public async Task<IActionResult> Get(bool includeTalks = false)
         {
             try
             {
                 var results = await _repository.GetAllCampsAsync(includeTalks);
-                return _mapper.Map<CampModel[]>(results);
+                var result = new
+                {
+                    Count = results.Length,
+                    Results = _mapper.Map<CampModel[]>(results)
+                };
+                return Ok(result);
             }
             catch (Exception)
             {
@@ -42,7 +46,6 @@ namespace CoreCodeCamp.Controllers
             }
         }
 
-        [MapToApiVersion("1.0")]
         [HttpGet("{moniker}")]
         public async Task<ActionResult<CampModel>> Get(string moniker)
         {
@@ -59,23 +62,6 @@ namespace CoreCodeCamp.Controllers
             }
         }
         
-        [MapToApiVersion("1.1")]
-        [HttpGet("{moniker}")]
-        public async Task<ActionResult<CampModel>> Get11(string moniker)
-        {
-            try
-            {
-                var result = await _repository.GetCampAsync(moniker, true);
-                if (result == null)
-                    return NotFound();
-                return _mapper.Map<CampModel>(result);
-            }
-            catch (Exception)
-            {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
-            }
-        }
-
         [HttpGet("search")]
         public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime theDate, bool includeTalks = false)
         {
--- src/Startup.cs
+++ src/Startup.cs
@@ -7,6 +7,7 @@ using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -21,10 +22,25 @@ namespace CoreCodeCamp
     {
       services.AddDbContext<CampContext>();
       services.AddScoped<ICampRepository, CampRepository>();
-      services.AddAutoMapper();
 
-      services.AddMvc()
-        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      var mappingConfig = new MapperConfiguration(mc =>
+      {
+          mc.AddProfile(new CampProfile());
+      });
+      var mapper = mappingConfig.CreateMapper();
+      services.AddSingleton(mapper);
+
+      services.AddApiVersioning(opt =>
+      {
+          opt.AssumeDefaultVersionWhenUnspecified = true;
+          opt.DefaultApiVersion = new ApiVersion(1, 1);
+          opt.ReportApiVersions = true;
+          opt.ApiVersionReader = ApiVersionReader.Combine(
+                  new HeaderApiVersionReader("X-Version"), 
+                  new QueryStringApiVersionReader("ver", "version"));
+      });
+      services.AddMvc(opt => opt.EnableEndpointRouting = false)
+          .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
     }
 
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
